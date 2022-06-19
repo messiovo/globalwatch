6e02658c50f3039c7f451949fdb3368c13cf8a6e
Messiovo <messiovo@gmail.com>
2022-06-19 08:01:02 +0100

    changes to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import {FaFacebookF} from 'react-icons/fa';
+import {AiOutlineTwitter, AiFillInstagram, AiOutlineCopyrightCircle} from 'react-icons/ai'
 
 const Footer = () => {
+  
   return (
-    <footer className='bg-black text-white flex flex-col items-center p-10'>
+    <footer className='bg-black text-white flex flex-col items-center p-10 min-h-screen justify-center'>
         <div>
           <span className='flex flex-col font-black text-2xl leading-6 uppercase text-left  cursor-pointer ml-10 items-center '>
               Global 
@@ -14,22 +17,26 @@ const Footer = () => {
               Catalogue
             </span>
           </span>
-          <p className='text-left p-10'>
+          <p className='text-center p-10'>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br/>
-               Fuga adipisci sequi fugit porro magni excepturi nobis ut conse.<br/>
-               repellat distinctio eos dignissimos aliquid officia numquam fugit?
+               Fuga adipisci sequi fugit porro magni excepturi nobis ut conseofficia numquam <br/>
+               repellat distinctio eos dignissimos aliquid fugit?
           </p>
           </div>
-        <div>
-            <Link to='./'><span>Contact Us</span></Link>
-            <Link to='./'><span>Terms of Service</span></Link>
-            <Link to='./'><span>Privacy Policy</span></Link>
-            <Link to='./'><span>Contact Us</span></Link>
+        <div className='flex flex-col px-10 sm:flex-row'>
+            <Link to='./'><span className='m-6 border-b-2 '>Contact Us</span></Link>
+            <Link to='./'><span  className='m-6 border-b-2 sm:my-6'>Terms of Service</span></Link>
+            <Link to='./'><span  className='m-6 border-b-2'>Privacy Policy</span></Link>
         </div>
-        <div>
-            social media links
+        <div className='flex pt-4'>
+            <Link to='./'><span><FaFacebookF size={30} className='m-6'/></span></Link>
+            <Link to='./'><span><AiFillInstagram size={30} className='m-6'/></span></Link>
+            <Link to='./'><span><AiOutlineTwitter size={30} className='m-6'/></span></Link>
         </div>
-
+        <div className='flex text-sm justify-end pt-20 w-full'>
+          <AiOutlineCopyrightCircle/>
+          <p>Messiovo & Udoka</p>
+        </div>
     </footer>
   )
 }
