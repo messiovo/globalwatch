82c750e00ecdcc072cb9e644ee0d8bbbdac89e4a
Messiovo <messiovo@gmail.com>
2022-06-19 16:22:33 +0100

    changes to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,18 +6,18 @@ import {AiOutlineArrowRight} from 'react-icons/ai';
 
  const Hero =() =>{
    return(
-     <div className='flex bg-[#252F46] w-full justify-between'>
-       <div className='w-2/4 text-white   pl-20 pt-20 leading-normal'>
+     <div className='flex flex-col bg-[#252F46] w-full justify-between lg:flex-row items-center'>
+       <div className=' text-white   px-20 pt-20 leading-normal pb-10 hero-content'>
          <h2 className='text-5xl font-bold'>Study <br/> Abroad <br/> Simplified!</h2>
          <p className='py-6'>There is a world out there full of possibilities.<br/>
          Our mission is to help you accomplish your dreams.</p>
          <Link to='/'>
-          <button className='flex items-center justify-center text-2xl rounded-full bg-[#C2956D] py-2 px-8'>
-           Learn more <span className='pl-2 mt-2'><AiOutlineArrowRight size={20}/></span>
+          <button className='flex items-center justify-center text-2xl rounded-full bg-[#C2956D] py-2 px-10 text-bold btn-hero'>
+           Learn more <span className='pl-2 mt-2 flow-root'><AiOutlineArrowRight size={20}/></span>
           </button>
          </Link>
        </div>
-       <div className='w-2/4'><img src={heropic} alt="" className='rounded 3xl'/></div>
+       <div className='hero-img'><img src={heropic} alt="" className='rounded 3xl img'/></div>
      </div>
    )
  }
