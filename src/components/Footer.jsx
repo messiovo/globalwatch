b6a7ce4b3cd83f008108d4ac5dadc548fea54fa9
Messiovo <messiovo@gmail.com>
2022-06-19 05:37:26 +0100

    chnages made to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <footer className='bg-black text-white flex flex-col items-center'>
+    <footer className='bg-black text-white flex flex-col items-center p-10'>
         <div>
-          <span className='flex flex-col font-black text-2xl leading-6 uppercase text-left  cursor-pointer ml-10 items-center mt-10'>
+          <span className='flex flex-col font-black text-2xl leading-6 uppercase text-left  cursor-pointer ml-10 items-center '>
               Global 
             <span>
                 Watch
@@ -13,11 +14,20 @@ const Footer = () => {
               Catalogue
             </span>
           </span>
-          <p className='text-center p-10'>
+          <p className='text-left p-10'>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br/>
-               Fuga adipisci sequi fugit porro magni excepturi nobis ut consequuntur quidem  Facilis suscipit dolores .<br/>
+               Fuga adipisci sequi fugit porro magni excepturi nobis ut conse.<br/>
                repellat distinctio eos dignissimos aliquid officia numquam fugit?
           </p>
+          </div>
+        <div>
+            <Link to='./'><span>Contact Us</span></Link>
+            <Link to='./'><span>Terms of Service</span></Link>
+            <Link to='./'><span>Privacy Policy</span></Link>
+            <Link to='./'><span>Contact Us</span></Link>
+        </div>
+        <div>
+            social media links
         </div>
 
     </footer>
