59eb77ec7cdd8490a8fb4b23c8bedcf40af7e37e
Messiovo <messiovo@gmail.com>
2022-06-20 16:27:04 +0100

    table changes

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -20,106 +20,109 @@ Our prices are very competitive. We make sure that you get expert advice at affo
         </p>
       </div>
       <div>
+      <div>  
+        <h3 className='text-center text-2xl font-semibold pb-4'>UK/European Visa Categories</h3>  
+        <table className='table'>
+          <tr>
+            <th>Visa Type</th>
+            <th>Purpose</th>
+          </tr>
+          <tr>
+            <td>Adult Dependant Visa</td>
+            <td>Foreign nationals requiring care from a relative who is a UK citizen</td>
+          </tr>
+          <tr>
+            <td>Adult Student Visa</td>
+            <td>Persons 18 and above Studying in the UK</td>
+          </tr>
+          <tr>
+            <td>Ancestry Visa</td>
+            <td>Citizens of commonwealth countries with a British grandparent</td>
+          </tr>
+          <tr>
+            <td>Business Visitor Visa</td>
+            <td>Travel to the UK for up to 6 months for business activities</td>
+          </tr>
+          <tr>
+            <td>Child Dependant Visa</td>
+            <td>Child of a British citizen/settled person not living an independent life</td>
+          </tr>
+          <tr>
+            <td>Child Student Visa</td>
+            <td>Children age 4-17 to study in the UK</td>
+          </tr>
+          <tr>
+            <td>Fiancés Visa</td>
+            <td>Fiancés of British Citizens/settled Persons citizen</td>
+          </tr>
+          <tr>
+            <td>Innovator Visa</td>
+            <td>Fiancés of British Citizens/settled Persons</td>
+          </tr>
+          <tr>
+            <td>Intra-Comapany Transfer Visa</td>
+            <td>Employees from multi-national companies transferring to work in a UK branch</td>
+          </tr>
+          <tr>
+            <td>Investor Visa</td>
+            <td>International investors with at least £2 million to invest in UK businesses</td>
+          </tr>
+          <tr>
+            <td>Ministry of Religion Visa</td>
+            <td>Work permit for religious workers</td>
+          </tr>
+          <tr>
+            <td>Paid Permitted Engagement visa</td>
+            <td>Short-term entry for individuals invited to the UK as experts in their field</td>
+          </tr>
+          <tr>
+            <td>Schengen Visa</td>
+            <td>Skilled Worker Visa</td>
+          </tr>
+          <tr>
+            <td>Skilled Worker Visa</td>
+            <td>Skilled Worker Visa</td>
+          </tr>
+          <tr>
+            <td>Sportsperson Visa</td>
+            <td>Work permit for elite sportspersons or qualified coaches</td>
+          </tr>
+        <tr>
+            <td>Spouse Visa</td>
+            <td>Work permit for elite sportspersons or qualified coaches</td>
+          </tr>
+        <tr>
+            <td>Startup Visa</td>
+            <td>Applicants with approved sponsors to start their first business in the UK</td>
+          </tr>
+        <tr>
+            <td>Student Visitor Visa</td>
+            <td>Studying in the UK on a temporary basis</td>
+          </tr>
+        <tr>
+            <td>Temporary Worker Visa</td>
+            <td>Temporary work, work experience and research in the UK for less than 12 months</td>
+          </tr>
+        <tr>
+            <td>Tourist Visa</td>
+            <td>Travel to the UK for up to 6 months for leisure or other purposes</td>
+          </tr>
+        <tr>
+            <td>Two year post-study work Visa</td>
+            <td>Work permit for international students who have studied a bachelor’s or master’s course in the UK</td>
+          </tr>
+        <tr>
+            <td>Unmarried Partner Visa</td>
+            <td>Long-term partner of British Citizens/settled persons</td>
+          </tr>
+        <tr>
+            <td>Youth MobilityVisa</td>
+            <td>2 year work permit for young people aged 18-20 from participating territories</td>
+          </tr>
         
-<table className='table'>
-  <tr>
-    <th>Visa Type</th>
-    <th>Purpose</th>
-  </tr>
-  <tr>
-    <td>Adult Dependant Visa</td>
-    <td>Foreign nationals requiring care from a relative who is a UK citizen</td>
-  </tr>
-  <tr>
-    <td>Adult Student Visa</td>
-    <td>Persons 18 and above Studying in the UK</td>
-  </tr>
-  <tr>
-    <td>Ancestry Visa</td>
-    <td>Citizens of commonwealth countries with a British grandparent</td>
-  </tr>
-  <tr>
-    <td>Business Visitor Visa</td>
-    <td>Travel to the UK for up to 6 months for business activities</td>
-  </tr>
-  <tr>
-    <td>Child Dependant Visa</td>
-    <td>Child of a British citizen/settled person not living an independent life</td>
-  </tr>
-  <tr>
-    <td>Child Student Visa</td>
-    <td>Children age 4-17 to study in the UK</td>
-  </tr>
-  <tr>
-    <td>Fiancés Visa</td>
-    <td>Fiancés of British Citizens/settled Persons citizen</td>
-  </tr>
-  <tr>
-    <td>Innovator Visa</td>
-    <td>Fiancés of British Citizens/settled Persons</td>
-  </tr>
-  <tr>
-    <td>Intra-Comapany Transfer Visa</td>
-    <td>Employees from multi-national companies transferring to work in a UK branch</td>
-  </tr>
-  <tr>
-    <td>Investor Visa</td>
-    <td>nternational investors with at least £2 million to invest in UK businesses</td>
-  </tr>
-  <tr>
-    <td>Ministry of Religion Visa</td>
-    <td>Work permit for religious workers</td>
-  </tr>
-  <tr>
-    <td>Paid Permitted Engagement visa</td>
-    <td>Short-term entry for individuals invited to the UK as experts in their field</td>
-  </tr>
-  <tr>
-    <td>Schengen Visa</td>
-    <td>Skilled Worker Visa</td>
-  </tr>
-  <tr>
-    <td>Skilled Worker Visa</td>
-    <td>Skilled Worker Visa</td>
-  </tr>
-  <tr>
-    <td>Sportsperson Visa</td>
-    <td>Work permit for elite sportspersons or qualified coaches</td>
-  </tr>
- <tr>
-    <td>Spouse Visa</td>
-    <td>Work permit for elite sportspersons or qualified coaches</td>
-  </tr>
- <tr>
-    <td>Startup Visa</td>
-    <td>Applicants with approved sponsors to start their first business in the UK</td>
-  </tr>
- <tr>
-    <td>Student Visitor Visa</td>
-    <td>Studying in the UK on a temporary basis</td>
-  </tr>
- <tr>
-    <td>Temporary Worker Visa</td>
-    <td>Temporary work, work experience and research in the UK for less than 12 months</td>
-  </tr>
- <tr>
-    <td>Tourist Visa</td>
-    <td>Travel to the UK for up to 6 months for leisure or other purposes</td>
-  </tr>
- <tr>
-    <td>Two year post-study work Visa</td>
-    <td>Work permit for international students who have studied a bachelor’s or master’s course in the UK</td>
-  </tr>
- <tr>
-    <td>Unmarried Partner Visa</td>
-    <td>Long-term partner of British Citizens/settled persons</td>
-  </tr>
- <tr>
-    <td>Youth MobilityVisa</td>
-    <td>2 year work permit for young people aged 18-20 from participating territories</td>
-  </tr>
- 
-</table>
+        </table>
+      </div> 
+
       </div>
 
     </div>
