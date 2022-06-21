154b5759b5b6c79b66cc7258202534635427289c
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2022-06-21 10:54:36 +0100

    removed reductant component and destructured props

--- src/components/CountriesCard.jsx
+++ src/components/CountriesCard.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 
-const CountriesCard = (props) => {
+const CountriesCard = ({ country }) => {
+  const { img, title } = country;
   return (
     <div className='w-60 h-28 bg-slate-200 m-4 '>
-      <Link to = './'><img className='w-60 h-28' src={props.country.img} alt="" />
-        <h2>{props.country.title}</h2>
+      <Link to = './'><img className='w-60 h-28' src={img} alt="" />
+        <h2>{title}</h2>
         </Link>
     </div>
   )
