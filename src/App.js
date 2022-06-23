f4facbb37ddad2fd5dd0b4338fc199b0336aa51b
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2022-06-23 10:31:02 +0100

    added country component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './styles/index.css';
 import { Navbar } from './components';
 import {Routes, Link, Route, BrowserRouter} from 'react-router-dom/index';
-import { Homepage } from './pages'
+import { Homepage, Country } from './pages'
 function App() {
   return (
       <div>
       <Navbar/>
       <Routes>
     <Route path='/' element={<Homepage />} />
-    <Route path='/country/:countryId' element={<Homepage />} />
+    <Route path='/country/:countryId' element={<Country />} />
   </Routes>
       </div>
   );
