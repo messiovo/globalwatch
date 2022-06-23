8e663a22f27a435472af865947eb287bd39e2ebe
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2022-06-23 12:21:04 +0100

    initialized slug

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import {useParams} from 'react-router-dom/index';
 const Country = () => {
+    const {countryId } = useParams();
   return (
       <div>
-          
+          {countryId }
     </div>
   )
 }
