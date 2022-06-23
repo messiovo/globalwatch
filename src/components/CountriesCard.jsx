a159e9065b86717a30c25d324c12129df617d5d7
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2022-06-23 10:45:59 +0100

    added dynamic linking in scountry card

--- src/components/CountriesCard.jsx
+++ src/components/CountriesCard.jsx
@@ -6,7 +6,7 @@ const CountriesCard = ({ country }) => {
   const { img, title } = country;
   return (
     <div className='w-60 h-40 bg-[#252F46] m-4 mx-6 rounded-lg card2'>
-      <Link to = './'><img className='w-60 h-28 rounded-t-lg' src={img} alt="" />
+      <Link to={`/country/${title}`}><img className='w-60 h-28 rounded-t-lg' src={img} alt="" />
         <h2 className='p-2 text-white text-center font-bold'>{title}</h2>
         </Link>
     </div>
