2743043701ebd7f4750ab010dc4afae32c81e8f5
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2022-06-24 20:04:19 +0100

    push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <div className='nav align-middle z-10'>
           <Link to="/"><span className='flex flex-col font-black text-2xl leading-6 uppercase text-left  cursor-pointer ml-10'>Global <span>Watch</span><span className='text-xs'>Catalogue</span></span></Link>
-      <ul className={!dropdown ? 'gap-12 lg:flex hidden float-right mr-[5rem] ml-auto my-auto': 'flex flex-col absolute left-0 w-full bg-slate-800 top-28 items-center gap-6'}>
+      <ul className={`${dropdown ? `h-0` : `h-full bg-slate-800 top-28`} lg:h-auto flex lg:flex-row flex-col my-auto align-middle overflow-y-hidden transition-all ease-in duration-1000 gap-12 ml-auto mr-auto lg:relative absolute w-full inset-0 items-center`}>
         <li>
           <Link to="/"><span className='hover:text-slate-300 transition-all duration-300 cursor-pointer'>Home</span></Link>
         </li>
@@ -21,13 +21,16 @@ const Navbar = () => {
           <Link to="/"><span className='hover:text-slate-300 transition-all duration-300 cursor-pointer'>About</span></Link>
         </li>
         <li>
-          <span className='hover:text-slate-300 transition-all duration-300 group'> 
-            <div className='items-center ml-12 flex'>Services <AiFillCaretDown className='mt-1.5 ml-1 group-hover:rotate-180'/></div> 
-          <ul className='flex flex-col top-24  bg-slate-600  min-h-full gap-4'>
+          <span className='transition-all duration-300 group flex flex-col items-center'> 
+            <div className='items-center flex group-hover:text-slate-300'>Services</div> 
+            <div className="w-36 h-36 bg-transparent absolute lg:flex hidden"></div>
+          <ul className='group-hover:h-fit flex flex-col top-28 bg-slate-600 lg:absolute gap-4 h-0 overflow-y-hidden group-hover:py-3  transition-all ease-in duration-1000 '>
             <li>
-              <Link to="/"><span className='m-6'>Immigration Services</span></Link>
-            </li> <li>
-              <Link to="/"><span className='m-6'>Immigration Services</span></Link>
+              <Link to="/"><span className='m-6 hover:text-slate-300/30'>Immigration Services</span></Link>
+              </li>
+              <li className='w-full h-2 bg-slate-300/30'></li>
+              <li>
+              <Link to="/"><span className='m-6 hover:text-slate-300/30'>Immigration Services</span></Link>
             </li>
           </ul>
           </span>
@@ -39,8 +42,8 @@ const Navbar = () => {
       {/* toggler */}
       <div className='my-auto ml-auto float-right lg:hidden cursor-pointer' >
         <div className="w-8 h-1 bg-white rounded-full 
-        after:w-8 after:h-1 after:content-[''] after:bg-white after:absolute after:-mt-2
-        before:w-8 before:h-1 before:content-[''] before:bg-white before:absolute before:mt-2
+        after:w-8 after:h-1 after:content-[''] after:bg-white after:absolute after:-mt-2 after:rounded-full
+        before:w-8 before:h-1 before:content-[''] before:bg-white before:absolute before:mt-2 before:rounded-full
         cursor-pointer
         "
         onClick={mobileHandler}
