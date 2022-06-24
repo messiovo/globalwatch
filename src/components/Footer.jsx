b8d07d922f8045050b7bd4311e6a7e9ca9630590
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2022-06-24 09:22:33 +0100

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import {AiOutlineTwitter, AiFillInstagram, AiOutlineCopyrightCircle} from 'react
 const Footer = () => {
   
   return (
-    <footer className='bg-[#252F46] text-white flex flex-col items-center p-10 justify-center'>
+    <div className='bg-[#252F46] text-white flex flex-col items-center p-6'>
         <div>
           <span className='flex flex-col font-black text-2xl leading-6 uppercase text-left  cursor-pointer ml-10 items-center '>
               Global 
@@ -17,26 +17,29 @@ const Footer = () => {
               Catalogue
             </span>
           </span>
-          <p className='text-center p-10'>
+          <p className='text-center'>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br/>
                Fuga adipisci sequi fugit porro magni excepturi nobis ut conseofficia numquam <br/>
                repellat distinctio eos dignissimos aliquid fugit?
           </p>
           </div>
-        <div className='flex flex-col px-10 gap-8  sm:flex-row'>
-            <Link to='./'><span className=' block '>Contact Us</span></Link>
-            <Link to='./'><span  className=' block '>Terms of Service</span></Link>
+        <div className='flex flex-col px-10 gap-8  lg:flex-row'>
+            <Link to='./'><span>Contact Us</span></Link>
+            <Link to='./'><span>Terms of Service</span></Link>
         </div>
-        <div className='flex pt-4'>
-            <Link to='./'><span><FaFacebookF size={30} className='m-6'/></span></Link>
-            <Link to='./'><span><AiFillInstagram size={30} className='m-6'/></span></Link>
-            <Link to='./'><span><AiOutlineTwitter size={30} className='m-6'/></span></Link>
+        <div className='flex pt-4 gap-4'>
+        {
+        // try using svg's instaed of these components
+        }
+            <Link to='./'><span><FaFacebookF size={30}/></span></Link>
+            <Link to='./'><span><AiFillInstagram size={30}/></span></Link>
+            <Link to='./'><span><AiOutlineTwitter size={30}/></span></Link>
         </div>
-        <div className='flex text-sm justify-end pt-20 w-full'>
+        <div className='flex text-sm pt-20 w-full'>
           <AiOutlineCopyrightCircle/>
           <p>Messiovo & Udoka</p>
         </div>
-    </footer>
+    </div>
   )
 }
 
