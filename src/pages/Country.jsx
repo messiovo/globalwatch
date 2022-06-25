95ecb50a8874bb8f784e0a4ca63f76a958c1f3cd
Udochukwuka Onyela <udochukwukaonyela@gmail.com>
2022-06-25 10:18:06 +0100

    fixes

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -51,28 +51,38 @@ const Country = () => {
         delivery
     } = visaRequirements;
   return (
-      <div>
-          {title}
-          {content}
-            {Location}
-            {Currency}
-            {Language}
-            {EnglishProficiency}
-            {WorkPermit}
-            {WorkingHours}
-            {averageIncome}
-            {costOfLiving}
-            {postStudyVisa}
-            {englishProficiencyReq}
-             {passport}
-            {photo}
-            {transcripts}
-            {proofOfEnglishProficiency}
-            
-    {addition}
-        {postTranscript}
-         {postCv}
-        {sop}
+      <div className='flex flex-col p-12'>
+          <h1>{title}</h1>
+          <p>{content}</p>
+            <p><span>Location:</span>{Location}</p>
+            <p><span>Currency:</span>{Currency}</p>
+            <p><span>Language:</span>{Language}</p>
+            <p><span>English Proficiency:</span>{EnglishProficiency}</p>
+            <p><span>Work Permit:</span>{WorkPermit}</p>
+            <p><span>Working Hours:</span>{WorkingHours}</p>
+            <p><span>Average Income:</span>{averageIncome}</p>
+            <p><span>Cost of Living:</span>{costOfLiving}</p>
+            <p><span>Post Study Visa</span>{postStudyVisa}</p>
+            <p><span>Dependency Visa</span>{englishProficiencyReq}</p>
+            <p><span>Average Tuitions</span>{passport}</p>
+            <div>
+            <h1>Applicationn Requirements </h1>
+              <div>
+                  <span>Under Graduate Study:</span>
+                  <p>{photo}</p>
+                  <p>{transcripts}</p>
+                  <p>{proofOfEnglishProficiency}</p>
+                  <span>Post Graduate Study:</span>
+                  <p>{addition}</p>
+                  <p>{postTranscript}</p>
+                  <p>{postCv}</p>
+                  <p>{sop}</p>
+   
+            </div>
+            </div>
+            <div>
+                <h1>Visa Requirements:</h1>
+            <div>
   {pic}
         {id}
         {cert}
@@ -84,9 +94,9 @@ const Country = () => {
         {birthCert}
         {langProf}
         {motLetter}
-        {delivery}
-        {test}
-          
+        {delivery}          
+    </div>
+    </div>
     </div>
   )
 }
