d65e93802cda2c9e334fe6870da874e49905c88d
udochukwuka onyela <udochukwukaonyela@gmail.com>
2022-06-27 15:21:35 +0100

    changes

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -52,51 +52,51 @@ const Country = () => {
         delivery
     } = visaRequirements;
   return (
-      <div className='flex flex-col p-24 gap-8'>
+      <div className='flex flex-col lg:p-24 p-6 gap-8'>
           {title && <h1 className='font-bold text-lg lg:text-xl text-center'>{title}</h1>}
-          {content && ~<p className='font-semibold text-center text-sm lg:text-lg'>{content}</p>}
-            {<div className="flex gap-6 text-sm lg:text-lg"><span className='text-lg font-semibold'>Location:</span><span>{Location}</span></div>}
-            {<div className="flex gap-6 text-sm lg:text-lg"><span className='text-lg font-semibold'>Currency:</span><span>{Currency}</span></div>}
-            {<div className="flex gap-6 text-sm lg:text-lg"><span className='text-lg font-semibold'>Language:</span><span>{Language}</span></div>}
-            {<div className="flex gap-6 text-sm lg:text-lg"><span className='text-lg font-semibold'>English Proficiency:</span><span>{EnglishProficiency}</span></div>}
-            {<div className="flex gap-6 text-sm lg:text-lg"><span className='text-lg font-semibold'>Work Permit:</span><span>{WorkPermit}</span></div>}
-            {<div className="flex gap-6 text-sm lg:text-lg"><span className='text-lg font-semibold'>Working Hours:</span><span>{WorkingHours}</span></div>}
-            {<div className="flex gap-6 text-sm lg:text-lg"><span className='text-lg font-semibold'>Average Income:</span><span>{averageIncome}</span></div>}
-            {<div className="flex gap-6 text-sm lg:text-lg"><span className='text-lg font-semibold'>Cost of Living:</span><span>{costOfLiving}</span></div>}
-            {<div className="flex gap-6 text-sm lg:text-lg"><span className='text-lg font-semibold'>Post Study Visa:</span><span className='w-3/4'>{postStudyVisa}</span></div>}
-            {<div className="flex gap-6 text-sm lg:text-lg"><span className='text-lg font-semibold'>Dependency Visa:</span><span>{englishProficiencyReq}</span></div>}
-          {<div className="flex gap-6 text-sm lg:text-lg"><span className='text-lg font-semibold'>Average Tuitions:</span><span>{AverageTuition}</span></div>}
+          {content && <p className='font-semibold text-center text-sm lg:text-lg w-full'>{content}</p>}
+            {Location && <div className="flex gap-6 text-xs lg:text-lg"><span className='text-sm font-semibold'>Location:</span><span>{Location}</span></div>}
+            {Currency && <div className="flex gap-6 text-xs lg:text-lg"><span className='text-sm font-semibold'>Currency:</span><span>{Currency}</span></div>}
+            {Language && <div className="flex gap-6 text-xs lg:text-lg"><span className='text-sm font-semibold'>Language:</span><span>{Language}</span></div>}
+            {EnglishProficiency  && <div className="flex gap-6 text-xs lg:text-lg"><span className='text-sm font-semibold'>English Proficiency:</span><span>{EnglishProficiency}</span></div>}
+            {WorkPermit  && <div className="flex gap-6 text-xs lg:text-lg"><span className='text-sm font-semibold'>Work Permit:</span><span>{WorkPermit}</span></div>}
+            {WorkingHours  && <div className="flex gap-6 text-xs lg:text-lg"><span className='text-sm font-semibold'>Working Hours:</span><span>{WorkingHours}</span></div>}
+            {averageIncome  && <div className="flex gap-6 text-xs lg:text-lg"><span className='text-sm font-semibold'>Average Income:</span><span>{averageIncome}</span></div>}
+            {costOfLiving  && <div className="flex gap-6 text-xs lg:text-lg"><span className='text-sm font-semibold'>Cost of Living:</span><span>{costOfLiving}</span></div>}
+            {postStudyVisa  && <div className="flex gap-6 text-xs lg:text-lg"><span className='text-sm font-semibold'>Post Study Visa:</span><span className='w-3/4'>{postStudyVisa}</span></div>}
+            {englishProficiencyReq && <div className="flex gap-6 text-xs lg:text-lg"><span className='text-sm font-semibold'>Dependency Visa:</span><span>{englishProficiencyReq}</span></div>}
+            {AverageTuition && <div className="flex gap-6 text-xs lg:text-lg"><span className='text-sm font-semibold'>Average Tuitions:</span><span>{AverageTuition}</span></div>}
           <div className='flex w-full gap-10 text-sm lg:text-lg'>
             <div className='font-semibold'>Application Requirements:</div>
               <div className='w-1/2'>
                   <span className='font-semibold'>Under Graduate Study:</span>
-                  {<p>{passport}</p>}
-                  {<p>{photo}</p>}
-                  {<p>{transcripts}</p>}
-                  {<p>{proofOfEnglishProficiency}</p>}
+              {passport  && <p>{passport}</p>}
+              {photo  && <p>{photo}</p>}
+              {transcripts  && <p>{transcripts}</p>}
+              {proofOfEnglishProficiency  && <p>{proofOfEnglishProficiency}</p>}
                   <span className='font-semibold'>Post Graduate Study:</span>
-                  {<p>{addition}</p>}
-                  {<p>{postTranscript}</p>}
-                  {<p>{postCv}</p>}
-                  {<p>{sop}</p>}
+              {addition  && <p>{addition}</p>}
+              {postTranscript && <p>{postTranscript}</p>}
+              {postCv && <p>{postCv}</p>}
+              {sop && <p>{sop}</p>}
    
             </div>
             </div>
           <div className='flex w-full gap-10 text-sm lg:text-lg'>
               <h1 className='font-semibold'>Visa Requirements:</h1>
               <div className='w-1/2'>
-        {<p>{pic}</p>}
-        {<p>{id}</p>}
-        {<p>{cert}</p>}
-        {<p>{financeproof}</p>}
-        {<p>{health}</p>}
-        {<p>{letter}</p>}
-        {<p>{cv}</p>}
-        {<p>{travelIns}</p>}
-        {<p>{birthCert}</p>}
-        {<p>{langProf}</p>}
-        {<p>{motLetter}</p>}
-        {<p>{delivery}</p>}
+        {pic  && <p>{pic}</p>}
+        {id  && <p>{id}</p>}
+        {cert && <p>{cert}</p>}
+        {financeproof  && <p>{financeproof}</p>}
+        {health && <p>{health}</p>}
+        {letter && <p>{letter}</p>}
+        {cv && <p>{cv}</p>}
+        {travelIns && <p>{travelIns}</p>}
+        {birthCert && <p>{birthCert}</p>}
+        {langProf && <p>{langProf}</p>}
+        {motLetter && <p>{motLetter}</p>}
+        {delivery && <p>{delivery}</p>}
     </div>
     </div>
     </div>
